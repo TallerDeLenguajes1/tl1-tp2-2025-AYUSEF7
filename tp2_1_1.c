74906bcbf10812ccaddcbdf7e9a9065c951d959d
“AYUSEF7” <yusefakd07@gmail.com>
2025-04-08 11:36:49 -0300

    inicial

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,11 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
 // codigo a completar 
-#define N 20 
+#define N 20
 
-int i;
-double vt[N];
-
-for(i = 0;i<N; i++) 
+int main()
 {
-    vt[i]=1+rand()%100;
-    printf(“%f   ”, vt[i]);
+    int i;
+    double vt[N];
+
+    srand(time(NULL));
+
+    for(i = 0;i<N; i++) 
+    {
+        vt[i]=1+rand()%100;
+        printf("%f   ", vt[i]);
+    }
+
+    return 0;
 }
